14e4f71f148fa08a9aafd4163819b8ebcb783fb0
StumblinGrumblin <gary@sweetlabs.com>
2015-02-11 17:58:09 -0800

    Index page restyle and seed file refactor

--- Gemfile
+++ Gemfile
@@ -6,9 +6,10 @@ gem 'rails', '4.1.8'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use SCSS for stylesheets
-gem 'sass-rails', '~> 4.0.3'
+gem 'sass-rails', '5.0.0.beta1'
+gem 'bootstrap-sass',           '3.2.0.0'
 # Use Uglifier as compressor for JavaScript assets
-gem 'uglifier', '>= 1.3.0'
+gem 'uglifier', '2.5.0'
 # Use CoffeeScript for .js.coffee assets and views
 gem 'coffee-rails', '~> 4.0.0'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
@@ -16,6 +17,7 @@ gem 'coffee-rails', '~> 4.0.0'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
+gem 'sass', '3.2.19'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,129 +6,48 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Whisky.delete_all
-Whisky.create!(name: 'Ardbeg 10',
+Whisky.create!(
+  name: 'Ardbeg 10',
   score: 85,
-  region:
-    %{<p>
-        Islay
-      </p>
-    },
-  color:
-    %{<p>
-        Straw
-      </p>
-    },
-  body:
-    %{<p>
-        Oily, some legs, mostly thin
-      </p>
-    },
-  nose:
-    %{<p>
-        Strong malt, soft peat
-      </p>
-    },
-  palatte:
-    %{<p>
-        Briney with a touch of liquorice
-      </p>
-    },
-  finish:
-    %{<p>
-        Slow, sweet, smokey
-      </p>
-    },
-  notes:
-    %{<p>
-        This expression is a fantastic and easy drinking Islay. It is easy to see why Ardbeg enjoys an almost cult following of enthusiests.
-      </p>
-    },
-    image_url: '2_95385750_2.jpg',
-    point_of_sale: 'Total Wines',
-    price: 48.99,
-    size: 750,
-    abv: 46)
+  region: 'Islay',
+  color: 'Straw',
+  body: 'Oily, some legs, mostly thin',
+  nose: 'Strong malt, soft peat',
+  palatte: 'Briney with a touch of liquorice',
+  finish: 'Slow, sweet, smokey',
+  notes: 'This expression is a fantastic and easy drinking Islay. It is easy to see why Ardbeg enjoys an almost cult following of enthusiests.',
+  image_url: '2_95385750_2.jpg',
+  point_of_sale: 'Total Wines',
+  price: 48.99,
+  size: 750,
+  abv: 46)
 
-Whisky.create!(name: 'Laphroaig 10',
+Whisky.create!(
+  name: 'Laphroaig 10',
   score: 88,
-  region:
-    %{<p>
-        Islay
-      </p>
-    },
-  color:
-    %{<p>
-        Medium Amber
-      </p>
-    },
-  body:
-    %{<p>
-        Medium body, no legs, not at all viscous
-      </p>
-    },
-  nose:
-    %{<p>
-        Salt spray, drift wood, medicinal
-      </p>
-    },
-  palatte:
-    %{<p>
-        Iodine, slight salt, smooth malt
-      </p>
-    },
-  finish:
-    %{<p>
-        Slow, warm, smokey
-      </p>
-    },
-  notes:
-    %{<p>
-        A little bit of water really opens up the bouquet. One of my favorite whiskies.
-      </p>
-    },
-    image_url: '3_3697750_2.jpg',
-    point_of_sale: 'Trader Joes',
-    price: 32.99,
-    size: 750,
-    abv: 43)
+  region: 'Islay',
+  color: 'Medium Amber',
+  body: 'Medium body, no legs, not at all viscous',
+  nose: 'Salt spray, drift wood, medicinal',
+  palatte: 'Iodine, slight salt, smooth malt',
+  finish: 'Slow, warm, smokey',
+  notes: 'A little bit of water really opens up the bouquet. One of my favorite whiskies.',
+  image_url: '3_3697750_2.jpg',
+  point_of_sale: 'Trader Joes',
+  price: 32.99,
+  size: 750,
+  abv: 43)
 
-Whisky.create!(name: 'Laphroaig Quarter Cask',
+Whisky.create!(
+  name: 'Laphroaig Quarter Cask',
   score: 90,
-  region:
-    %{<p>
-        Islay
-      </p>
-    },
-  color:
-    %{<p>
-        Sunlit amber shade
-      </p>
-    },
-  body:
-    %{<p>
-        Lightly viscous with slow legs.
-      </p>
-    },
-  nose:
-    %{<p>
-        Salt spray, oak
-      </p>
-    },
-  palatte:
-    %{<p>
-        Smokey, almond, iodine, briney
-      </p>
-    },
-  finish:
-    %{<p>
-        Slow, soft wood, smokey
-      </p>
-    },
-  notes:
-    %{<p>
-        A little bit of water really opens up the bouquet. Takes everything I like about Laphroaig 10 and kicks it up a notch.
-      </p>
-    },
+  region:'Islay',
+  color: 'Sunlit amber shade',
+  body: 'Lightly viscous with slow legs.',
+  nose: 'Salt spray, oak',
+  palatte: 'Smokey, almond, iodine, briney',
+  finish: 'Slow, soft wood, smokey',
+  notes: 'A little bit of water really opens up the bouquet. Takes everything I like about Laphroaig 10 and kicks it up a notch.',
     image_url: '2_103349750_2.jpg',
     point_of_sale: 'K&L Wines',
     price: 54.99,
@@ -137,41 +56,13 @@ Whisky.create!(name: 'Laphroaig Quarter Cask',
 
 Whisky.create!(name: 'Lagavulin 16',
   score: 92,
-  region:
-    %{<p>
-        Islay
-      </p>
-    },
-  color:
-    %{<p>
-        Antique Copper
-      </p>
-    },
-  body:
-    %{<p>
-        Medium, lots of slow legs
-      </p>
-    },
-  nose:
-    %{<p>
-        Peat smoke, light tar, citrus, tangerine, faint maple
-      </p>
-    },
-  palatte:
-    %{<p>
-        Sweet citrus, Smokey
-      </p>
-    },
-  finish:
-    %{<p>
-        Long smooth finish
-      </p>
-    },
-  notes:
-    %{<p>
-        One of the finest if Islay expressions being bottled today.
-      </p>
-    },
+  region: 'Islay',
+  color: 'Antique Copper',
+  body: 'Medium, lots of slow legs',
+  nose: 'Peat smoke, light tar, citrus, tangerine, faint maple',
+  palatte: 'Sweet citrus, Smokey',
+  finish: 'Long smooth finish',
+  notes: 'One of the finest if Islay expressions being bottled today.',
     image_url: '2_643750_2.jpg',
     point_of_sale: 'Costco',
     price: 68.99,
