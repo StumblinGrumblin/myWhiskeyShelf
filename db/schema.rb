414154dc6f4514136189c2df17990d89cb78acde
StumblinGrumblin <gary@sweetlabs.com>
2015-03-17 11:28:33 -0700

    Added additional whiskies

--- db/schema.rb
+++ db/schema.rb
@@ -13,20 +13,20 @@
 
 ActiveRecord::Schema.define(version: 20150209224204) do
 
-  create_table "whiskies", force: true do |t|
-    t.string   "name"
-    t.string   "region"
-    t.string   "color"
-    t.string   "body"
-    t.string   "nose"
-    t.string   "palatte"
-    t.string   "finish"
-    t.string   "notes"
-    t.string   "image_url"
-    t.string   "point_of_sale"
-    t.decimal  "price",         precision: 6, scale: 2
+  create_table "whiskies", force: :cascade do |t|
+    t.string   "name",          limit: 255
+    t.string   "region",        limit: 255
+    t.string   "color",         limit: 255
+    t.string   "body",          limit: 255
+    t.string   "nose",          limit: 255
+    t.string   "palatte",       limit: 255
+    t.string   "finish",        limit: 255
+    t.string   "notes",         limit: 255
+    t.string   "image_url",     limit: 255
+    t.string   "point_of_sale", limit: 255
+    t.decimal  "price",                     precision: 6, scale: 2
     t.integer  "size"
-    t.decimal  "abv",           precision: 2, scale: 2
+    t.decimal  "abv",                       precision: 2, scale: 2
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "score"
